fa594951f7f1f6d2f9a65e50d4acea6a0a6bdbde
坂田裕二 <yuuji@MacBook-Air.local>
2020-08-18 01:47:42 +0900

    edit post form and create updatecommentview

--- board/views.py
+++ board/views.py
@@ -2,6 +2,7 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView
 from django.views.generic import DetailView
 from django.views.generic import CreateView
+from django.views.generic import UpdateView
 from .forms import CommentForm
 from .models import Comment
 
@@ -20,3 +21,22 @@ class CreateCommentView(CreateView):
     model = Comment
     form_class = CommentForm
     success_url = reverse_lazy('comments:allcomments')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = 'コメントの投稿'
+        context['form_name'] = 'コメントの投稿'
+        context['button_label'] = 'コメントを投稿する'
+        return context
+
+class UpdateCommentView(UpdateView):
+    model = Comment
+    form_class = CommentForm
+    success_url = reverse_lazy('comments:allcomments')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = 'コメントの更新'
+        context['form_name'] = 'コメントの更新'
+        context['button_label'] = 'コメントを更新する'
+        return context
